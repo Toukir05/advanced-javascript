d09283d8aea8bfce22f834eda49838df61ea3d24
Toukir05 <67517095+Toukir05@users.noreply.github.com>
2020-08-09 22:32:44 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//False
+//false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truth 
+// '0', ' ', []  
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
