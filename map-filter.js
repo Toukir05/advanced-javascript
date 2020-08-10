537c9a36da94099f549099e211aa025ec275f6d9
Toukir05 <67517095+Toukir05@users.noreply.github.com>
2020-08-11 02:52:21 +0600

    map-filter done

---
+++ map-filter.js
@@ -0,0 +1,25 @@
+const number = [3, 4, 5, 6, 7, 8]
+// const output = [];
+
+// for(let i = 0; i < number.length; i++){
+//     const element = number[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// function square (element){
+//     return element * element;
+// }
+
+// const result = number.map(function(element){
+//     return element * element;
+// })
+// console.log(result);
+
+// const result = number.map(x => x * x);
+
+// const bigger = number.filter( x => x > 5);
+
+const isThere = number.find(x => x > 5);
+
+console.log(isThere);
